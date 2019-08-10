2f86724f1922b7c20543cbfe059cdfa12eba5976
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-09 23:31:11 -0500

    Initial Commit

---
+++ NumberTable2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumberTable2
+//
+//  Created by IMCS on 8/9/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
